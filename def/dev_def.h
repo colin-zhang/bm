e83cfea09b933497ca377611fac4c5b3511ddd9d
colin-zhang <ingm@foxmail.com>
2016-12-13 09:11:15 +0800

    likeliy

--- def/dev_def.h
+++ def/dev_def.h
@@ -33,4 +33,12 @@
 
 #define dev_print_dbg dbg_Print
 
+#ifdef __GNUC__
+#define likely(x)   __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#else
+#define likely(x)   !!(x)
+#define unlikely(x) !!(x)
+#endif
+
 #endif
